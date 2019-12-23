751a8d4e853a63097bb62d17a6ff87e0897f2e7d
Manuel Behrendt <mabe@Riemann.local>
2019-12-23 11:20:12 +0100

    First commit

---
+++ src/Mera.jl
@@ -0,0 +1,32 @@
+module Mera
+
+# ==================================================================
+# Read/Save and process large AMR/particle data sets
+# of hydrodynamic simulations with Julia!
+#
+# Manuel Behrendt, since 2017
+# Max-Planck-Institute for Extraterrestrial Physics, Garching
+# Ludwig-Maximillians-University, Munich
+#
+# https://github.com/ManuelBehrendt/Mera.jl
+#
+# Credits:
+# The RAMSES-files reader are strongly influenced
+# by Romain Teyssier's amr2map.f90 and part2mapf.90
+# ==================================================================
+
+println()
+println( "*__   __ _______ ______   _______ ")
+println( "|  |_|  |       |    _ | |   _   |")
+println( "|       |    ___|   | || |  |_|  |")
+println( "|       |   |___|   |_||_|       |")
+println( "|       |    ___|    __  |       |")
+println( "| ||_|| |   |___|   |  | |   _   |")
+println( "|_|   |_|_______|___|  |_|__| |__|")
+println()
+
+
+include("types.jl")
+
+
+end # module
