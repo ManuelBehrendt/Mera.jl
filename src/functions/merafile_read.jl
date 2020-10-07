8b09202a7ec65c3766668e248c315d48288ebb3b
Manuel Behrendt <universe@manuelbehrendt.com>
2020-10-07 23:00:20 +0200

    Add reader for particle,clump data to mera-files

--- src/functions/merafile_read.jl
+++ src/functions/merafile_read.jl
@@ -38,6 +38,14 @@ function read_merafile(;
         dtype = HydroDataType()
         if verbose println() end
         printtime("Get hydro data: ", verbose)
+    elseif datatype == :particles
+        dtype = PartDataType()
+        if verbose println() end
+        printtime("Get particle data: ", verbose)
+    elseif datatype == :clumps
+        dtype = ClumpDataType()
+        if verbose println() end
+        printtime("Get clump data: ", verbose)
     end
 
 
@@ -68,18 +76,23 @@ function read_merafile(;
         data = group["data"]
         merafile_version = read(group["merafile_version"])
         column_names = read(data["names"])
-        t = table([read(data[i]) for i in column_names]...,
-        names=Symbol.(column_names), pkey=[:cx, :cy, :cz], presorted = false)
 
+        if datatype == :hydro
+            t = table([read(data[i]) for i in column_names]...,
+            names=Symbol.(column_names), pkey=[:cx, :cy, :cz], presorted = false)
+        elseif datatype == :particles
+            t = table([read(data[i]) for i in column_names]...,
+            names=Symbol.(column_names), pkey=[:x, :y, :z, :id], presorted = false)
+        elseif datatype == :clumps
+            t = table([read(data[i]) for i in column_names]...,
+            names=Symbol.(column_names), presorted = false)
+        end
 
-        # Fill HydroDataType
+        # Fill datatype
         dtype.info = info
         dtype.data = t
-        dtype.lmin = read(group["lmin"])
-        dtype.lmax = read(group["lmax"])
         dtype.boxlen = read(group["boxlen"])
         dtype.ranges = read(group["ranges"])
-        dtype.selected_hydrovars = read(group["selected_hydrovars"])
 
         desc = read(group["used_descriptors"])
         if desc == 0
@@ -87,10 +100,22 @@ function read_merafile(;
         else
             dtype.used_descriptors = desc
         end
-        dtype.smallr = read(group["smallr"])
-        dtype.smallc = read(group["smallc"])
         dtype.scale = info.scale
 
+
+        if datatype == :hydro # Fill specific HydroDataType fields
+            dtype.lmin = read(group["lmin"])
+            dtype.lmax = read(group["lmax"])
+            dtype.selected_hydrovars = read(group["selected_hydrovars"])
+            dtype.smallr = read(group["smallr"])
+            dtype.smallc = read(group["smallc"])
+        elseif datatype == :particles # Fill specific PartDataType fields
+            dtype.lmin = read(group["lmin"])
+            dtype.lmax = read(group["lmax"])
+            dtype.selected_partvars = read(group["selected_partvars"])
+        elseif datatype == :clumps # Fill specific ClumpDataType fields
+            dtype.selected_clumpvars = read(group["selected_clumpvars"])
+        end
 
         printtablememory(dtype, verbose)
 
