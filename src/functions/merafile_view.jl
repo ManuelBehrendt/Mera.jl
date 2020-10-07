732296239dd757162d62424a767ec217c405d69d
Manuel Behrendt <universe@manuelbehrendt.com>
2020-10-07 20:55:08 +0200

    Add view for user content in mera-file; Add verbose mode

--- src/functions/merafile_view.jl
+++ src/functions/merafile_view.jl
@@ -1,5 +1,4 @@
 
-# todo verbose mode
 function view_merafile(filename::String;
                         verbose::Bool=verbose_mode)
 
@@ -12,58 +11,63 @@ function view_merafile(;
                        verbose::Bool=verbose_mode)
 
     viewoutput = Dict()
-    println("Overview Stored Data:")
-    println()
+
+    if verbose
+        println("Mera-file Contains:")
+        println()
+    end
 
     file = h5open(filename, "r")
     stored_datatypes = names(file)
     for datatype in stored_datatypes
-        output = readheader(file, datatype)
+        output = readheader(file, datatype, verbose)
         viewoutput[datatype] = output
     end
 
     s = filesize(filename)
     svalue, sunit = humanize(Float64(s), 3, "memory")
     viewoutput["FileSize"] = (svalue, sunit)
-    println("Total file size: ", svalue, " ", sunit)
+
+    if verbose
+        println("Total file size: ", svalue, " ", sunit)
+        println("-----------------------------------")
+        println()
+    end
 
     close(file)
     return viewoutput
 end
 
 
-# todo test furhter content in HDF5 file
-function readheader(file, datatype::String)
+function readheader(file, datatype::String, verbose::Bool)
     output = Dict()
 
     if known_datatype(datatype)
-        println(datatype)
-        println("-----------")
         group = file[datatype]
         path = group["info/path"]
         path = read(path)
         output["path"] = path
-        println("Directory: ", path )
+
 
         merafile_version = group["merafile_version"]
         merafile_version = read(merafile_version)
         output["merafile_version"] = merafile_version
-        println("merafile_version: ", merafile_version)
+
 
         simcode = group["info/simcode"]
         simcode = read(simcode)
         output["SimCode"] = simcode
-        println("Simulation code: ", simcode )
+
 
         column_names = group["data/names"]
         column_names = Symbol.(read(column_names) )
         output["Variables"] = column_names
-        println("Data variables: ", column_names )
+
 
         total_fields = group["total_fields"]
         total_fields = read(total_fields)
         output["TotalFields"] = total_fields
-        println("Total fields: ", total_fields)
+
 
         ctype = group["compression_type"]
         ctype = read(ctype)
@@ -71,25 +75,44 @@ function readheader(file, datatype::String)
         cn = read(cn)
         output["CompressionType"] = ctype
         output["Compression"] = cn
-        println("Compression type: ", ctype, " with: ", cn )
+
 
         mem = group["memory"]
         mem = read(mem)
         mem1 = parse(Float64, mem[1])
         output["Memory"] = mem
-        println("Uncompressed memory size: ", round(mem1, digits=3)," ", mem[2] )
+
 
         comment = group["comment"]
         comment = read(comment)
         output["Comment"] = comment
-        println("Comment: ", comment)
-        println()
-        println()
+
+        if verbose
+            println(datatype)
+            println("-----------")
+            println("Directory: ", path )
+            println("merafile_version: ", merafile_version)
+            println("Simulation code: ", simcode )
+            println("Data variables: ", column_names )
+            println("Total fields: ", total_fields)
+            println("Compression type: ", ctype, " with: ", cn )
+            println("Uncompressed memory size: ", round(mem1, digits=3)," ", mem[2] )
+            println("Comment: ", comment)
+            println()
+            println()
+        end
     else
-        println("Further content: ", datatype)
-        output["ContentType"] = typeof(read(group))
-        println()
-        println()
+
+
+
+        group = file[datatype]
+        output[datatype] = typeof(read(group))
+        if verbose
+            println("Further content: ", datatype)
+            println("-----------")
+            println()
+            println()
+        end
     end
 
     return output
