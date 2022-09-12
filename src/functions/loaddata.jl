5000404389b83f13580792c9421d755d3b369139
Manuel Behrendt <universe@manuelbehrendt.com>
2022-09-12 15:11:29 +0200

    Specify JLD2 load function

--- src/functions/loaddata.jl
+++ src/functions/loaddata.jl
@@ -1,28 +1,28 @@
-function loaddata(output::Int; path::String="./", 
+function loaddata(output::Int; path::String="./",
                     fname = "output_",
                     datatype::Symbol,
                     verbose::Bool=true)
-    
+
     if verbose
         println("Open Mera-file:")
         println()
     end
-    
+
     filename = outputname(fname, output) * ".jld2"
     fpath    = checkpath(path, filename)
-    
-        
+
+
     # get root-list with datatypes
     f = jldopen(fpath)
     froot = f.root_group
     fkeys = keys(froot.written_links)
     close(f)
-    
+
     # todo: check if request exists
     dlink = string(datatype) * "/data"
-    dataobject = load(fpath, dlink)
+    dataobject = JLD2.load(fpath, dlink)
 
     #printtablememory(dataobject, verbose)
-    
+
     return dataobject
 end
