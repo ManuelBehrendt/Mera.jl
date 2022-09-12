3797be7737babcf22f70be49a4ea6f25e38ddd99
Manuel Behrendt <universe@manuelbehrendt.com>
2022-09-12 21:29:17 +0200

    Check selected datatype

--- src/functions/data_info.jl
+++ src/functions/data_info.jl
@@ -1,6 +1,6 @@
 function infodata(output::Int; path::String="./",
                     fname = "output_",
-                    datatype::Symbol,
+                    datatype::Any=nothing,
                     verbose::Bool=true)
 
     printtime("",verbose)
@@ -8,8 +8,26 @@ function infodata(output::Int; path::String="./",
     filename = outputname(fname, output) * ".jld2"
     fpath    = checkpath(path, filename)
 
-    # todo: check if request exists
-    inflink = string(datatype) * "/info"
+
+    # get root-list with datatypes
+    #filename = fpath * "L1_Zlib_0019.jld2"
+    f = jldopen(fpath)
+    froot = f.root_group
+    fkeys = keys(froot.written_links)
+    close(f)
+
+    # check if request exists
+    if datatype == nothing
+        dtype = fkeys[1]
+    else
+        if datatype in fkeys
+            dtype = datatype
+        else
+            error("Datatype $datatype does not exist...")
+        end
+    end
+    println("Use datatype: ", dtype)
+    inflink = string(dtype) * "/info"
     dataobject = JLD2.load(fpath, inflink)
 
     printsimoverview(dataobject, verbose) # print overview on screen
