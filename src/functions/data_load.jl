dcd60585232ebb807a3c292ad8b6626b8824c168
Manuel Behrendt <universe@manuelbehrendt.com>
2022-09-13 15:31:47 +0200

    Add subregion selection for function: loaddata

--- src/functions/data_load.jl
+++ src/functions/data_load.jl
@@ -1,6 +1,13 @@
 function loaddata(output::Int; path::String="./",
                     fname = "output_",
                     datatype::Symbol,
+
+                    xrange::Array{<:Any,1}=[missing, missing],
+                    yrange::Array{<:Any,1}=[missing, missing],
+                    zrange::Array{<:Any,1}=[missing, missing],
+                    center::Array{<:Any,1}=[0., 0., 0.],
+                    range_unit::Symbol=:standard,
+
                     verbose::Bool=true)
 
     printtime("",verbose)
@@ -13,6 +20,15 @@ function loaddata(output::Int; path::String="./",
         println()
     end
 
+    info = infodata(output, path=path,
+                        fname = fname,
+                        datatype=datatype,
+                        verbose=false)
+    #------------------
+    # convert given ranges and print overview on screen
+    ranges = prepranges(info, range_unit, verbose, xrange, yrange, zrange, center)
+    #------------------
+
     # get root-list with datatypes
     f = jldopen(fpath)
     froot = f.root_group
@@ -23,7 +39,17 @@ function loaddata(output::Int; path::String="./",
     dlink = string(datatype) * "/data"
     dataobject = JLD2.load(fpath, dlink)
 
-    #printtablememory(dataobject, verbose)
+    # filter selected data region
+    dataobject = subregion(dataobject, :cuboid,
+                     xrange=xrange,
+                     yrange=yrange,
+                     zrange=zrange,
+                     center=center,
+                     range_unit=range_unit,
+                     verbose=false)
+
+
+    printtablememory(dataobject, verbose)
 
     return dataobject
 end
