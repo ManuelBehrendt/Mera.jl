0cc6d5ad95751c8e792ac08fd9098e01217ac095
Manuel Behrendt <universe@manuelbehrendt.com>
2023-03-30 19:01:06 +0200

    Revert "Extend output of new hydro projection with effective resolution and pixel size"
    This reverts commit a6ad1da184001426bace2e66125c869ba02d77ee.

--- src/types.jl
+++ src/types.jl
@@ -461,8 +461,6 @@ mutable struct HydroMapsType <: DataMapsType
     extent::Array{Float64,1}
     cextent::Array{Float64,1}
     ratio::Float64
-    effres::Int
-    pixsize::Float64
     boxlen::Float64
     smallr::Float64
     smallc::Float64
