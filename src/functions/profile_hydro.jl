a43ee87de6cedd1853f126a4925e07366a3e767b
Manuel Behrendt <universe@manuelbehrendt.com>
2023-04-27 20:14:42 +0200

    Add function: profile (hydro)

---
+++ src/functions/profile_hydro.jl
@@ -0,0 +1,42 @@
+
+
+
+
+
+function profile(  dataobject::HydroDataType,
+                    vars::Array{Symbol,1},
+                    units::Array{Symbol,1},
+                    hrange::AbstractRange{<:Number};
+                    closed::Symbol=:left,
+                    weighting::Symbol=:mass,
+                    mapping::Symbol=:cell, #projection
+                    center::Array{<:Any,1}=[0., 0., 0.],
+                    center_unit::Symbol=:standard,
+                    direction::Symbol=:z,
+                    mask::MaskType=[false],
+                    checkbins::Bool=false,
+                    verbose::Bool=true)
+
+    center = Mera.center_in_standardnotation(dataobject.info, center, center_unit)
+
+    xquantity = getvar(dataobject, vars[1], units[1], center=center, center_unit=center_unit, mask=mask)
+    yquantity = getvar(dataobject, vars[2], units[2], center=center, center_unit=center_unit, mask=mask)               
+    wquantity = getvar(dataobject, weighting, center=center, center_unit=center_unit, mask=mask) 
+
+    xhist = fit(Histogram, xquantity, weights(yquantity .* wquantity),  hrange )
+    whist = fit(Histogram, xquantity, weights(wquantity),  hrange )
+    profile = xhist.weights ./ whist.weights # weighted average
+
+    if closed==:left
+        radius = hrange[1:end-1]
+    elseif closed==:right
+        radius = hrange[2:end]
+    end
+
+    if checkbins==true
+        Nhist = fit(Histogram, xquantity,  hrange )
+        return radius, profile, Nhist.weights
+    else
+        return radius, profile
+    end
+end
