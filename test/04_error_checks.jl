4b198b3c924e8fd3fcc3517efc8a112ed48efb4b
Manuel Behrendt <universe@manuelbehrendt.com>
2023-04-30 14:52:54 +0200

    update @test_throws test

--- test/04_error_checks.jl
+++ test/04_error_checks.jl
@@ -13,22 +13,21 @@ function checktypes_error(output, path, datatype)
     info.sinks      = false
 
     if datatype == :hydro
-        @test gethydro(info)
+        gethydro(info)
     elseif datatype == :particles
-        @test getparticles(info)
+        getparticles(info)
     elseif datatype == :gravity
-        @test getgravity(info)
+        getgravity(info)
     elseif datatype == :clumps
-        @test getclumps(info)
+        getclumps(info)
     elseif datatype == :amr
-        @test Mera.checkfortype(info, :amr)
+        Mera.checkfortype(info, :amr)
     elseif datatype == :rt
-        @test Mera.checkfortype(info, :rt)
+        Mera.checkfortype(info, :rt)
     elseif datatype == :sinks
-        @test Mera.checkfortype(info, :sinks)
+        Mera.checkfortype(info, :sinks)
     end
-
-    return true
+    return 
 end
 
 
@@ -37,11 +36,11 @@ end
 function checklevelmax_error(output, path)
     info = getinfo(output, path, verbose=false)
     gas = gethydro(info, lmax=10)
-    return true
+    return 
 end
 
 function checklevelmin_error(output, path)
     info = getinfo(output, path, verbose=false)
     gas = gethydro(info, lmax=1)
-    return true
+    return 
 end
