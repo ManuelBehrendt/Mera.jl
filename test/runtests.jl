2f8a74b062e7c125e19902879cabef7fead98ad0
Manuel Behrendt <universe@manuelbehrendt.com>
2023-04-30 14:53:43 +0200

    update @test_throws

--- test/runtests.jl
+++ test/runtests.jl
@@ -504,17 +504,18 @@ output = 2
         printstyled("--------------------------------------\n", color=:cyan)
 
 
-        @test_throws checktypes_error(output, path, :hydro)
-        @test_throws checktypes_error(output, path, :particles)
-        @test_throws checktypes_error(output, path, :gravity)
-        @test_throws checktypes_error(output, path, :rt)
-        @test_throws checktypes_error(output, path, :clumps)
-        @test_throws checktypes_error(output, path, :sinks)
-        @test_throws checktypes_error(output, path, :amr)
+        
+        @test_throws ErrorException("[Mera]: Simulation has no hydro files!") checktypes_error(output, path, :hydro)
+        @test_throws ErrorException("[Mera]: Simulation has no particle files!") checktypes_error(output, path, :particles)
+        @test_throws ErrorException("[Mera]: Simulation has no gravity files!") checktypes_error(output, path, :gravity)
+        @test_throws ErrorException("[Mera]: Simulation has no rt files!") checktypes_error(output, path, :rt)
+        @test_throws ErrorException("[Mera]: Simulation has no clump files!") checktypes_error(output, path, :clumps)
+        @test_throws ErrorException("[Mera]: Simulation has no sink files!") checktypes_error(output, path, :sinks)
+        @test_throws ErrorException("[Mera]: Simulation has no amr files!") checktypes_error(output, path, :amr)
 
-
-        @test_throws checklevelmax_error(output, path)
-        @test_throws checklevelmin_error(output, path)
+        @test_throws ErrorException("[Mera]: Simulation lmax=7 < your lmax=10") checklevelmax_error(output, path)
+        @test_throws ErrorException("[Mera]: Simulation lmin=3 > your lmin=1") checklevelmin_error(output, path)
+        
 
     end
 end
