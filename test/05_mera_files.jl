9fa386cbc3a2160873954957b51c10241a554b18
Manuel Behrendt <universe@manuelbehrendt.com>
2023-05-07 22:03:20 +0200

    update test for converting mera files and comparing with stored info data

--- test/05_mera_files.jl
+++ test/05_mera_files.jl
@@ -1,6 +1,105 @@
 
 
 function convert_jld2(output, path)
+    st = convertdata(output, [:hydro], path=path)
+    vd = viewdata(output)
+    stdata = keys(vd)
+    flag1 = in("hydro", stdata)
+    println("flag1: hydro in jld2 file? ", flag1)
+    println()
+
+    st = convertdata(output, :hydro, path=path)
+    vd = viewdata(output)
+    stdata = keys(vd)
+    flag2 = in("hydro", stdata)
+    println("flag2: hydro in jld2 file? ", flag2)
+    println()
+
     st = convertdata(output, path=path, fpath=".")
-    return true
+    vd = viewdata(output)
+    stdata = keys(vd)
+    flag3 = in("hydro", stdata)
+    flag4 = in("gravity", stdata)
+    flag5 = in("partticles", stdata)
+    println("flag3: hydro in jld2 file? ", flag3)
+    println("flag4: gravity in jld2 file? ", flag4)
+    println("flag5: particles in jld2 file? ", flag5)
+    println()
+    return flag1 == true && flag2 == true && flag3 == true && flag4 == true && flag5 == true
+end
+
+
+function info_jld2(output, path)
+    info = getinfo(output, path)
+    infoconv = infodata(output)
+
+
+    info_fields = propertynames(info)
+    infoconv_fields = propertynames(infoconv);
+    field_comparison = true
+    field_comparison_data = true
+    for i in info_fields
+        fieldef = isdefined(infoconv, i)
+        
+        if !fieldef
+            field_comparison = false
+            println("missing field: ", i)
+        else
+            field_data_infoconv = getfield(infoconv, i)
+            field_data_info = getfield(info, i)
+            
+            if i == :fnames
+                comparefields(info.fnames, infoconv.fnames, field_comparison_data)
+                
+            elseif i == :descriptor
+                comparefields(info.descriptor, infoconv.descriptor, field_comparison_data)
+                
+            elseif i == :files_content
+                comparefields(info.files_content, infoconv.files_content, field_comparison_data)
+                
+            elseif i == :scale
+                comparefields(info.scale, infoconv.scale, field_comparison_data)
+                
+            elseif i == :grid_info
+                comparefields(info.grid_info, infoconv.grid_info, field_comparison_data)
+                
+            elseif i == :part_info
+                comparefields(info.part_info, infoconv.part_info, field_comparison_data)
+                
+            elseif i == :compilation
+                comparefields(info.compilation, infoconv.compilation, field_comparison_data)
+                
+            elseif i == :constants
+                comparefields(info.constants, infoconv.constants, field_comparison_data)
+
+            else
+            
+                compare_field_data = field_data_infoconv == field_data_info
+                if !compare_field_data
+                    field_comparison_data = false
+                    println("unequal data - field: ", i)
+                end
+            end
+
+        end
+    end
+
+
+
+    return  field_comparison == true && field_comparison_data == true
+end
+
+
+function comparefields(info_field, infoconv_field, field_comparison_data)
+    inames = propertynames(info_field)
+    for j in inames
+        field_inames_infoconv = getfield(infoconv_field, j)
+        field_inames_info = getfield(info_field, j)
+        compare_field_inames = field_inames_infoconv == field_inames_info
+        if !compare_field_inames
+            field_comparison_data = false
+            println("unequal data - field: ", j)
+        end
+    end
+    return
 end
