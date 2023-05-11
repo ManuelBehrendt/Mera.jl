54f12e1031489faad79d00e4141086925c809112
Manuel Behrendt <universe@manuelbehrendt.com>
2023-05-11 15:14:35 +0200

    more space for printscreen (runtests)

--- test/screen_output.jl
+++ test/screen_output.jl
@@ -1,6 +1,9 @@
 function printscreen(message)
     println()
+    println()
+    println()
     printstyled("--------------------------------------\n", color=:cyan)
     @info(message)
     printstyled("--------------------------------------\n", color=:cyan) 
+    println()
 end
