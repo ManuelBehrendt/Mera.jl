e754f8d5729f8453bc3e03fdef4f53a723a7a336
Manuel Behrendt <universe@manuelbehrendt.com>
2023-05-13 01:23:19 +0200

    introduce tests for simulations with clump files

---
+++ test/clumptests.jl
@@ -0,0 +1,31 @@
+
+
+
+@testset "01 General Tests" begin
+    include("clumps/general.jl")
+end
+
+# verbose(false)
+# showprogress(false)
+# @testset "02 getvar clumps" begin
+#     printscreen("getvar clumps:")
+#     include("clumps/values_clumps.jl")
+# end
+
+# @testset "03 getvar clumps" begin
+#     printscreen("getvar clumps:")
+#     include("clumps/values_clumps.jl")
+# end
+
+# end
+
+# verbose(true)
+# @testset  "04 MERA files" begin
+#     printscreen("Write/Read MERA files:")
+#     include("clumps/merafiles.jl")
+# end
+
+# @testset "05 Error Checks" begin
+#     printscreen("data types:")
+#     include("clumps/errors.jl")
+# end
