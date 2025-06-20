73fc98d8e9b1ac4d7df8aab9442a2024c124ba26
Manuel Behrendt <universe@manuelbehrendt.com>
2025-06-20 21:17:37 +0200

    activate interpolations

--- src/Mera.jl
+++ src/Mera.jl
@@ -33,7 +33,8 @@ using ProgressMeter
 using StatsBase
 using OnlineStats
 using ImageTransformations
-#using ImageTransformations.Interpolations
+using ImageTransformations.Interpolations
+#using ImageFiltering
 
 using JLD2, CodecZlib, CodecBzip2, CodecLz4
 using TimerOutputs
