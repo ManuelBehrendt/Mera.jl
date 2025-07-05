9973e9b94e6ba7042b5285388e578ece6fdb81ae
ManuelBehrendt <universe@manuelbehrendt.com>
2025-07-05 12:28:24 +0200

    fix function name

--- src/read_data/RAMSES/gethydro.jl
+++ src/read_data/RAMSES/gethydro.jl
@@ -349,11 +349,11 @@ function gethydro(dataobject::InfoType;
     
     # Read hydro data using optimized multithreaded I/O
     if read_cpu
-        vars_1D, pos_1D, cpus_1D = gethydrodata_deprecated(dataobject, length(nvarh_list),
+        vars_1D, pos_1D, cpus_1D = gethydrodata(dataobject, length(nvarh_list),
                                      nvarh_corr, lmax, ranges,
                                      print_filenames, show_progress, verbose, read_cpu, isamr, max_threads)
     else
-        vars_1D, pos_1D = gethydrodata_deprecated(dataobject, length(nvarh_list),
+        vars_1D, pos_1D = gethydrodata(dataobject, length(nvarh_list),
                                     nvarh_corr, lmax, ranges,
                                      print_filenames, show_progress, verbose, read_cpu, isamr, max_threads)
     #                                                    ↑ ADD verbose HERE
