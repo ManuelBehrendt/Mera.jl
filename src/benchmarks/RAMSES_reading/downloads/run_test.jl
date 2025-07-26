32accc894bebf2a45ea5a774ef636c499064ac61
ManuelBehrendt <universe@manuelbehrendt.com>
2025-07-26 11:59:36 +0200

    make only zip file downloadable

--- src/benchmarks/RAMSES_reading/downloads/run_test.jl
+++
@@ -1,6 +0,0 @@
-import Pkg; Pkg.activate(".")
-using Mera
-
-path = "/simulation/folder" # RAMSES simulation folder (outputs)
-output_number = 250 # number of the RAMSES snapshot
-run_reading_benchmark(output_number, path)
