c41e4f0fc0992fa4f7aa39ae7ce958b015f4c343
ManuelBehrendt <universe@manuelbehrendt.com>
2025-08-06 16:27:07 +0200

    feat: Enhanced particle analysis with comprehensive angular momentum calculations
    ✨ New Features:
    - Added 17 new particle variables for rotational dynamics analysis
    - Comprehensive angular momentum calculations (L = mass × h)
    - Multi-coordinate system support (Cartesian, cylindrical, spherical)
    - Robust coordinate transformations with singularity handling
    
    🔧 New Particle Variables:
    - Specific angular momentum: hx, hy, hz, h
    - Total angular momentum: lx, ly, lz, l
    - Cylindrical components: lr_cylinder, lϕ_cylinder
    - Spherical components: lr_sphere, lθ_sphere, lϕ_sphere
    - Spherical velocities: vr_sphere, vθ_sphere, vϕ_sphere
    - Angular position: ϕ (azimuthal angle)

--- src/functions/getvar_particles.jl
+++ src/functions/getvar_particles.jl
@@ -141,6 +141,58 @@ function get_data(dataobject::PartDataType,
                                                                   (p[bpos] - center[2] * boxlen )^2 +
                                                                   (p[cpos] - center[3] * boxlen )^2 )  )
 
+       # Spherical velocity components
+       elseif i == :vr_sphere
+           x = getvar(dataobject, :x, center=center)
+           y = getvar(dataobject, :y, center=center)
+           z = getvar(dataobject, :z, center=center)
+           vx = getvar(dataobject, :vx)
+           vy = getvar(dataobject, :vy)
+           vz = getvar(dataobject, :vz)
+
+           selected_unit = getunit(dataobject, :vr_sphere, vars, units)
+           r_sphere = @. sqrt(x^2 + y^2 + z^2)
+           vr = @. (x * vx + y * vy + z * vz) / r_sphere * selected_unit
+           vr[isnan.(vr)] .= 0. # handle r = 0
+           vars_dict[:vr_sphere] = vr
+
+       elseif i == :vθ_sphere
+           x = getvar(dataobject, :x, center=center)
+           y = getvar(dataobject, :y, center=center)
+           z = getvar(dataobject, :z, center=center)
+           vx = getvar(dataobject, :vx)
+           vy = getvar(dataobject, :vy)
+           vz = getvar(dataobject, :vz)
+
+           selected_unit = getunit(dataobject, :vθ_sphere, vars, units)
+           r_sphere = @. sqrt(x^2 + y^2 + z^2)
+           r_cylinder2 = @. x^2 + y^2
+           numerator = @. z * (x * vx + y * vy) - r_cylinder2 * vz
+           vtheta = @. numerator / (r_sphere * sqrt(r_cylinder2)) * selected_unit
+           vtheta[isnan.(vtheta)] .= 0. # handle singularities
+           vars_dict[:vθ_sphere] = vtheta
+
+       elseif i == :vϕ_sphere
+           x = getvar(dataobject, :x, center=center)
+           y = getvar(dataobject, :y, center=center)
+           vx = getvar(dataobject, :vx)
+           vy = getvar(dataobject, :vy)
+
+           selected_unit = getunit(dataobject, :vϕ_sphere, vars, units)
+           r_cylinder = @. sqrt(x^2 + y^2)
+           vphi = @. (x * vy - y * vx) / r_cylinder * selected_unit
+           vphi[isnan.(vphi)] .= 0. # handle r = 0
+           vars_dict[:vϕ_sphere] = vphi
+
+       # Azimuthal angle
+       elseif i == :ϕ
+           x = getvar(dataobject, :x, center=center)
+           y = getvar(dataobject, :y, center=center)
+
+           selected_unit = getunit(dataobject, :ϕ, vars, units)
+           phi = @. atan(y, x) * selected_unit  # atan2 function
+           vars_dict[:ϕ] = phi
+
 
 
 
@@ -158,6 +210,139 @@ function get_data(dataobject::PartDataType,
         elseif i == :age
             selected_unit = getunit(dataobject, :age, vars, units)
             vars_dict[:age] = ( ref_time .- getvar(dataobject, :birth) ) .* selected_unit
+
+        # Specific angular momentum calculations (h = r × v)
+        elseif i == :hx # specific angular momentum x-component
+            selected_unit = getunit(dataobject, :hx, vars, units)
+            y = getvar(dataobject, :y, center=center)
+            z = getvar(dataobject, :z, center=center)
+            vy = getvar(dataobject, :vy)
+            vz = getvar(dataobject, :vz)
+            vars_dict[:hx] = (y .* vz .- z .* vy) .* selected_unit
+
+        elseif i == :hy # specific angular momentum y-component
+            selected_unit = getunit(dataobject, :hy, vars, units)
+            x = getvar(dataobject, :x, center=center)
+            z = getvar(dataobject, :z, center=center)
+            vx = getvar(dataobject, :vx)
+            vz = getvar(dataobject, :vz)
+            vars_dict[:hy] = (z .* vx .- x .* vz) .* selected_unit
+
+        elseif i == :hz # specific angular momentum z-component
+            selected_unit = getunit(dataobject, :hz, vars, units)
+            x = getvar(dataobject, :x, center=center)
+            y = getvar(dataobject, :y, center=center)
+            vx = getvar(dataobject, :vx)
+            vy = getvar(dataobject, :vy)
+            vars_dict[:hz] = (x .* vy .- y .* vx) .* selected_unit
+
+        elseif i == :h # specific angular momentum magnitude
+            selected_unit = getunit(dataobject, :h, vars, units)
+            hx = getvar(dataobject, :hx, center=center)
+            hy = getvar(dataobject, :hy, center=center)
+            hz = getvar(dataobject, :hz, center=center)
+            vars_dict[:h] = sqrt.(hx .^2 .+ hy .^2 .+ hz .^2) .* selected_unit
+
+        # Angular momentum calculations (L = mass × specific angular momentum)
+        elseif i == :lx # angular momentum x-component
+            selected_unit = getunit(dataobject, :lx, vars, units)
+            mass = getvar(dataobject, :mass)
+            hx = getvar(dataobject, :hx, center=center)
+            vars_dict[:lx] = mass .* hx .* selected_unit
+
+        elseif i == :ly # angular momentum y-component
+            selected_unit = getunit(dataobject, :ly, vars, units)
+            mass = getvar(dataobject, :mass)
+            hy = getvar(dataobject, :hy, center=center)
+            vars_dict[:ly] = mass .* hy .* selected_unit
+
+        elseif i == :lz # angular momentum z-component
+            selected_unit = getunit(dataobject, :lz, vars, units)
+            mass = getvar(dataobject, :mass)
+            hz = getvar(dataobject, :hz, center=center)
+            vars_dict[:lz] = mass .* hz .* selected_unit
+
+        elseif i == :l # angular momentum magnitude
+            selected_unit = getunit(dataobject, :l, vars, units)
+            mass = getvar(dataobject, :mass)
+            h_magnitude = getvar(dataobject, :h, center=center)
+            vars_dict[:l] = mass .* h_magnitude .* selected_unit
+
+        # Cylindrical angular momentum components
+        elseif i == :lr_cylinder # radial angular momentum (cylindrical)
+            selected_unit = getunit(dataobject, :lr_cylinder, vars, units)
+            mass = getvar(dataobject, :mass)
+            lx = getvar(dataobject, :lx, center=center)
+            vars_dict[:lr_cylinder] = lx .* selected_unit
+
+        elseif i == :lϕ_cylinder # azimuthal angular momentum (cylindrical)
+            selected_unit = getunit(dataobject, :lϕ_cylinder, vars, units)
+            mass = getvar(dataobject, :mass)
+            
+            x = getvar(dataobject, :x, center=center)
+            y = getvar(dataobject, :y, center=center)
+            vx = getvar(dataobject, :vx)
+            vy = getvar(dataobject, :vy)
+            
+            r_cylinder = @. sqrt(x^2 + y^2)
+            vphi = @. (x * vy - y * vx) / r_cylinder
+            vphi[isnan.(vphi)] .= 0. # handle r = 0
+            
+            l_phi = @. mass * r_cylinder * vphi * selected_unit
+            l_phi[isnan.(l_phi)] .= 0. # handle r = 0
+            vars_dict[:lϕ_cylinder] = l_phi
+
+        # Spherical angular momentum components  
+        elseif i == :lr_sphere # radial angular momentum (spherical)
+            selected_unit = getunit(dataobject, :lr_sphere, vars, units)
+            mass = getvar(dataobject, :mass)
+            
+            x = getvar(dataobject, :x, center=center)
+            y = getvar(dataobject, :y, center=center)
+            z = getvar(dataobject, :z, center=center)
+            vx = getvar(dataobject, :vx)
+            vy = getvar(dataobject, :vy)
+            vz = getvar(dataobject, :vz)
+            
+            r_sphere = @. sqrt(x^2 + y^2 + z^2)
+            vr = @. (x * vx + y * vy + z * vz) / r_sphere
+            vr[isnan.(vr)] .= 0. # handle r = 0
+            
+            vars_dict[:lr_sphere] = mass .* r_sphere .* vr .* selected_unit
+
+        elseif i == :lθ_sphere # polar angular momentum (spherical)
+            selected_unit = getunit(dataobject, :lθ_sphere, vars, units)
+            mass = getvar(dataobject, :mass)
+            
+            x = getvar(dataobject, :x, center=center)
+            y = getvar(dataobject, :y, center=center)
+            z = getvar(dataobject, :z, center=center)
+            vx = getvar(dataobject, :vx)
+            vy = getvar(dataobject, :vy)
+            vz = getvar(dataobject, :vz)
+            
+            r_sphere = @. sqrt(x^2 + y^2 + z^2)
+            r_cylinder2 = @. x^2 + y^2
+            numerator = @. z * (x * vx + y * vy) - r_cylinder2 * vz
+            vtheta = @. numerator / (r_sphere * sqrt(r_cylinder2))
+            vtheta[isnan.(vtheta)] .= 0. # handle singularities
+            
+            vars_dict[:lθ_sphere] = mass .* r_sphere .* vtheta .* selected_unit
+
+        elseif i == :lϕ_sphere # azimuthal angular momentum (spherical)
+            selected_unit = getunit(dataobject, :lϕ_sphere, vars, units)
+            mass = getvar(dataobject, :mass)
+            
+            x = getvar(dataobject, :x, center=center)
+            y = getvar(dataobject, :y, center=center)
+            vx = getvar(dataobject, :vx)
+            vy = getvar(dataobject, :vy)
+            
+            r_cylinder = @. sqrt(x^2 + y^2)
+            vphi = @. (x * vy - y * vx) / r_cylinder
+            vphi[isnan.(vphi)] .= 0. # handle r = 0
+            
+            vars_dict[:lϕ_sphere] = mass .* r_cylinder .* vphi .* selected_unit
         end
 
 
