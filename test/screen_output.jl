d13fe12a93e7f63d3d0949175ef5fbe380cc0c0f
ManuelBehrendt <universe@manuelbehrendt.com>
2025-08-08 16:08:32 +0200

    deleted or reorganized files

--- test/screen_output.jl
+++
@@ -1,9 +0,0 @@
-function printscreen(message)
-    println()
-    println()
-    println()
-    printstyled("--------------------------------------\n", color=:cyan)
-    @info(message)
-    printstyled("--------------------------------------\n", color=:cyan) 
-    println()
-end
